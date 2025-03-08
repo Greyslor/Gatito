a2d5b7fe597056f6bda7c825fbb3974041fa1c65
Bl4ke36 <3du4rdo.pv@gmail.com>
2025-03-07 22:30:49 -0600

    coneccion

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,22 @@ using UnityEngine.Networking;
 
 public class GameManager : MonoBehaviour
 {
+    public string id;
+    public int box;
+    public string json;
+    public GatoData data;
+
+    public GameObject playerSelection;
+    public GameObject game;
     void Start()
     {
-        StartCoroutine(GetText());
+        playerSelection.SetActive(true);
+        game.SetActive(false);
     }
 
-    IEnumerator GetText()
+    IEnumerator ResetInfo()
     {
-        UnityWebRequest www = UnityWebRequest.Get("http://www.my-server.com");
+        UnityWebRequest www = UnityWebRequest.Get("http://localhost/gato/gato.php?action=1");
         yield return www.Send();
 
         if (www.isNetworkError)
@@ -21,11 +29,77 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            // Show results as text
-            Debug.Log(www.downloadHandler.text);
+            print("Database reset");
+
 
             // Or retrieve results as binary data
             byte[] results = www.downloadHandler.data;
         }
+    }
+
+    IEnumerator GetInfo()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://localhost/gato/gato.php?action=2&id="+id);
+        yield return www.Send();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            json = www.downloadHandler.text;
+            data = JsonUtility.FromJson<GatoData>(json);
+            print(data);
+
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+
+    IEnumerator PlayerMove()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://localhost/gato/gato.php?action=3&id=" + id + "&pos=" + box);
+        yield return www.Send();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            print(www.downloadHandler.text);
+
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+
+    public void SelectId(string idInput)
+    {
+        id = idInput;
+        playerSelection.SetActive(false);
+        game.SetActive(true);
+
+    }
+
+    public void PlayerMove(int boxInput)
+    {
+        box = boxInput;
+        StartCoroutine(PlayerMove());
+    }
+    [ContextMenu("reset")]
+    public void ResetGame()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ResetInfo());
+    }
+
+    [ContextMenu("getinfo")]
+    public void GetInfoFunc()
+    {
+        StartCoroutine(GetInfo());
     }
 }
